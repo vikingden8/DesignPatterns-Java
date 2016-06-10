8a3ac621a5afac619c7b9a1ab487fca8e27acb20
viking den <vikingden@live.com>
2016-06-11 00:25:28 +0800

    添加通用的Log输出类

---
+++ src/com/viking/util/Log.java
@@ -0,0 +1,12 @@
+package com.viking.util;
+
+/**
+ * Created by Viking Den on 2016/6/10.
+ */
+public class Log {
+
+    public static void info(String tag , String msg){
+        System.out.println(tag + " : " + msg);
+    }
+
+}
